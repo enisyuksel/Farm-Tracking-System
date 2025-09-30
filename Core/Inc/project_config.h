82a8f04a36cea47a56249f666f42320fdc22e9f0
yukselen26 <yukselenis26@gmail.com>
2025-09-30 10:24:05 +0300

    Remove unnecessary files and code related to system calls, memory management, startup, linker script, and debug configuration for STM32WL55JCIX project.

--- Core/Inc/project_config.h
+++ Core/Inc/project_config.h
@@ -4,7 +4,7 @@
  * @author  IQ Yazılım
  * @brief   Project Configuration from config.json
  * @note    This file is auto-generated from config.json
- * @date    Generated: 2025-09-30 10:13:34
+ * @date    Generated: 2025-09-30 10:23:11
  ******************************************************************************
  */
 
